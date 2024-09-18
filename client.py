fe345d07c8fa7c0e340f54cb761049765185e980
JBerkheiser <91573629+JBerkheiser@users.noreply.github.com>
2024-09-17 22:37:20 -0400

    HW2 complete

--- client.py
+++ client.py
@@ -7,10 +7,17 @@ s2 = socket.socket()
 serverIP = input('What is the servers IP address?')
 s2.connect((serverIP, serverPort))
 
-data = s2.recv(1024).decode()
-print("Received " + data)
+data = s2.recv(1024).decode('utf-8')
+print(data)
 
-retMessage = 'Received'
-s2.sendall(retMessage.encode())
+while True:
+    serverRequest = input("Enter a string to send to the server")
+    if(serverRequest == ''):
+        break
+    else:
+        s2.sendall(serverRequest.encode('utf-8'))
+    
+    data = s2.recv(1024).decode()
+    print(data)
 
 s2.close()
--- server.py
+++ server.py
@@ -1,17 +1,42 @@
 import socket
 import datetime
+from threading import Thread
 
-ip = '127.0.0.1'
-port = 5000
+def main():
+    ip = '127.0.0.1'
+    port = 5000
+    clientNumber = 0
 
-s1 = socket.socket()
-s1.bind((ip, port))
-print("The server is running")
-s1.listen()
-connection, address = s1.accept()
 
-currentDateTime = datetime.datetime.now().strftime("%a %b %d %H:%M:%S %Z %Y")
-connection.sendall(currentDateTime.encode())
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s1.bind((ip, port))
+    print("The server is running")
+    s1.listen()
 
-message = connection.recv(1024).decode()
-print(message)
+    while True:
+        connection, address = s1.accept()
+        clientNumber += 1
+        clientThread = Thread(target = handleClient, args = (connection, address, clientNumber))
+        clientThread.start()
+
+def handleClient(client_socket, client_address, client_value):  
+    welcomeMessage = "Hello, you are client #" + str(client_value)
+    client_socket.sendall(welcomeMessage.encode('utf-8'))
+
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
+            if(message.lower() == 'time'):
+                responseMessage = datetime.datetime.now().strftime("%a %b %d %H:%M:%S %Z %Y")
+            else:
+                responseMessage = message.upper()
+            client_socket.sendall(responseMessage.encode())
+        except ConnectionResetError:
+            break
+    print("Client #",client_value," disconnected")
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
