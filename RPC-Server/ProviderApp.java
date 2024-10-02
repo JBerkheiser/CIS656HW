2353aaf903de7f3dab6923dbb855178e6934cacf
JBerkheiser <91573629+JBerkheiser@users.noreply.github.com>
2024-10-01 21:16:50 -0400

    Added threading

--- RPC-Server/ProviderApp.java
+++ RPC-Server/ProviderApp.java
@@ -16,42 +16,64 @@ public class ProviderApp {
 
     private void begin() throws IOException 
     {
-        ServerSocket listener = new ServerSocket(9090);
-        System.out.println("The server is running");
-      
-        while (true) 
+        try(ServerSocket listener = new ServerSocket(9090))
         {
-            Socket socket = listener.accept();
-            try 
+            System.out.println("The server is running");
+      
+            while (true) 
             {
-                // 
-                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-                Object object = objectInputStream.readObject();
-
-                // 
-                String result = "";
-                if (object instanceof CalculateRpcRequest) 
-                {
-                    CalculateRpcRequest calculateRpcRequest = (CalculateRpcRequest) object;
-                    if("time".equals(calculateRpcRequest.getMessage())) 
-                    {
-                        LocalDateTime now = LocalDateTime.now();
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                        result = now.format(formatter);
-                    } 
-                    else
-                    {
-                        result = calculateRpcRequest.getMessage().toUpperCase();
-                    }
-                }
-
-                // 
-                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-                objectOutputStream.writeObject(new String(result));
-            } catch(Exception e) 
-            {
-            	System.out.println("error!");
-            } 
+                Socket socket = listener.accept();
+                ClientHandler clientHandler = new ClientHandler(socket);
+                new Thread(clientHandler).start();
+            }
+        } catch(IOException e)
+        {
+            e.printStackTrace();
         }
-    } 
+
+    }
+}
+
+class ClientHandler implements Runnable
+{
+    private final Socket socket;
+
+    public ClientHandler(Socket clientSocket)
+    {
+        this.socket = clientSocket;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            Object object = objectInputStream.readObject();
+
+            // 
+            String result = "";
+            if (object instanceof CalculateRpcRequest) 
+            {
+                CalculateRpcRequest calculateRpcRequest = (CalculateRpcRequest) object;
+                if("time".equals(calculateRpcRequest.getMessage())) 
+                {
+                    LocalDateTime now = LocalDateTime.now();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    result = now.format(formatter);
+                } 
+                else
+                {
+                    result = calculateRpcRequest.getMessage().toUpperCase();
+                }
+            }
+
+            // 
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject(new String(result));
+        } catch(Exception e) 
+        {
+            System.out.println("error!");
+        } 
+    }
 }
