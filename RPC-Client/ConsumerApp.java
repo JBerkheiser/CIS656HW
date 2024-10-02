199e13e05814443847589466bb98969e70e180f8
JBerkheiser <91573629+JBerkheiser@users.noreply.github.com>
2024-10-01 21:31:49 -0400

    Fixed consumer app

--- RPC-Client/ConsumerApp.java
+++ RPC-Client/ConsumerApp.java
@@ -6,15 +6,19 @@ public class ConsumerApp {
     public static void main(String[] args) throws IOException {
         Messager messager = new MessagerRemoteImpl();
         Scanner scan = new Scanner(System.in);
-        String input = scan.next();
+        String input;
         String result = "";
 
-        do {
+        while(true)
+        {
+            input = scan.nextLine();
+            if(input.trim().isEmpty())
+            {
+                break;
+            }
             result = messager.messaging(input);
             System.out.println(result);
-            input = scan.next();
-        } while(!input.isEmpty());   
-
+        }
         scan.close();
     }
 }
